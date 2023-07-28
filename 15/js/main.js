e10424a4ef25721488c5886e3a0a43cb2bc8a5ec
Keks <keks@htmlacademy.ru>
2023-07-28 13:31:13 +0300

    :heavy_check_mark: Сборка #15

---
+++ 15/js/main.js
@@ -0,0 +1,31 @@
+import {setupFormValidation, unblockSubmitButton, setFormSubmit} from './form.js';
+import './scale.js';
+import './effects.js';
+import { showAlert} from './utils.js';
+import { getData, sendData} from './api.js';
+import { showFilters, setDebouncedSort } from './filters.js';
+import { previewPicture } from './upload.js';
+import {showSuccessMessage, showErrorMessage} from './message.js';
+
+setupFormValidation();
+
+setFormSubmit(async (data) => {
+  try {
+    await sendData(data);
+    showSuccessMessage();
+  } catch {
+    showErrorMessage();
+  } finally {
+    unblockSubmitButton();
+  }
+});
+
+try {
+  const data = await getData();
+  showFilters();
+  previewPicture();
+  setDebouncedSort(data);
+} catch(err) {
+  showAlert(err.message);
+}
+
---
+++ 15/js/thumbnail.js
@@ -0,0 +1,46 @@
+import { getTemplate, renderPack} from './utils.js';
+import { showBigPicture } from './big-picture.js';
+import { getPhotoById, photos } from './data.js';
+
+const template = getTemplate('picture');
+const container = document.querySelector('.pictures');
+
+
+/**
+ * @param {Event} evt
+ */
+const onThumbnailClick = (evt) => {
+  evt.preventDefault();
+  const thumbnail = evt.currentTarget;
+  const id = +thumbnail.dataset.thumbnailId + 1;
+  const picture = getPhotoById(id);
+
+  showBigPicture(picture);
+};
+
+
+const renderThumbnail = ({comments, description, likes, url, id}) => {
+  const thumbnail = template.cloneNode(true);
+
+  const imageElement = thumbnail.querySelector('.picture__img');
+  imageElement.src = url;
+  imageElement.alt = description;
+
+  thumbnail.querySelector('.picture__comments').textContent = comments.length;
+  thumbnail.querySelector('.picture__likes').textContent = likes;
+  thumbnail.dataset.thumbnailId = id;
+
+  return thumbnail;
+};
+
+const setupThumbnails = (pictureData) => {
+  const thumbnail = renderThumbnail(pictureData);
+  thumbnail.addEventListener('click', onThumbnailClick);
+  return thumbnail;
+};
+
+const renderThumbnails = (pictures) => renderPack(container, pictures, setupThumbnails);
+
+renderThumbnails(photos);
+
+export {renderThumbnails};
